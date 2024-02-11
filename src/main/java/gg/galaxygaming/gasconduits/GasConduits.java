6691dc8a53b896f565cb625b23bc4a53961ebf13
serenibyss <10861407+serenibyss@users.noreply.github.com>
2024-02-10 23:08:27 -0600

    fix missed version check

--- src/main/java/gg/galaxygaming/gasconduits/GasConduits.java
+++ src/main/java/gg/galaxygaming/gasconduits/GasConduits.java
@@ -1,7 +1,5 @@
 package gg.galaxygaming.gasconduits;
 
-import java.util.Map;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -11,9 +9,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.NetworkCheckHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.apache.commons.lang3.tuple.Triple;
 import org.apache.logging.log4j.Logger;
@@ -38,23 +33,6 @@ import info.loenwind.autoconfig.ConfigHandler;
      acceptedMinecraftVersions = GasConduitsConstants.MC_VERSION)
 @Mod.EventBusSubscriber(modid = GasConduitsConstants.MOD_ID)
 public class GasConduits implements IEnderIOAddon {
-
-    @NetworkCheckHandler
-    @SideOnly(Side.CLIENT)
-    public boolean checkModLists(Map<String, String> modList, Side side) {
-        /*
-         * On the client when showing the server list: Require the mod to be there and of the same version.
-         *
-         * On the client when connecting to a server: Require the mod to be there. Version check is done on the server.
-         *
-         * On the server when a client connects: Standard Forge version checks with a nice error message apply.
-         *
-         * On the integrated server when a client connects: Require the mod to be there and of the same version. Ugly
-         * error message.
-         */
-        return modList.keySet().contains(GasConduitsConstants.MOD_ID) &&
-                GasConduitsConstants.VERSION.equals(modList.get(GasConduitsConstants.MOD_ID));
-    }
 
     private static final @Nonnull String DEFAULT_DEPENDENCIES = "after:" + crazypants.enderio.base.EnderIO.MODID;
     public static final @Nonnull String DEPENDENCIES = DEFAULT_DEPENDENCIES;
--- src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
+++ src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
@@ -1,10 +1,12 @@
 package gg.galaxygaming.gasconduits;
 
+import crazypants.enderio.api.EIOTags;
+
 public class GasConduitsConstants {
 
     public static final String MOD_ID = "gasconduits";
     public static final String MOD_NAME = "GasConduits";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = EIOTags.VERSION;
     public static final String MC_VERSION = "1.12.2,";
 
     public static final int GAS_VOLUME = 1000;
