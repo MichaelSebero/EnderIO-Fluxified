4804f144ff7480160b3fb608670860206fb3bb67
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2024-10-26 16:20:35 -0400

    Updated

--- src/main/java/crazypants/enderio/zoo/config/package-info.java
+++
@@ -1,4 +0,0 @@
-@ParametersAreNonnullByDefault // Not the right one, but eclipse knows only 3 null annotations anyway, so it's ok
-package crazypants.enderio.zoo.config;
-
-import javax.annotation.ParametersAreNonnullByDefault;
